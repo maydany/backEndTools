6586560d2bbbaaec623a5e777cd307fe398bdb01
maydany <ishlove89@naver.com>
2018-08-18 00:22:10 +0900

    feat: Add get function for home page

--- nodejs/nodejs_practice/index.js
+++ nodejs/nodejs_practice/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.get('/', (req, res) =>
+{res.send('Hello home page')});
 app.listen(3000, () => {
   console.log('Connected 3000 port!')
 });
