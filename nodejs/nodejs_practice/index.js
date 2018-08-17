71a7d93d95e271bb321665901d12441d9fd4875a
maydany <ishlove89@naver.com>
2018-08-18 01:38:37 +0900

    docs: Update main page

--- nodejs/nodejs_practice/index.js
+++ nodejs/nodejs_practice/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) =>
-{res.send('Hello home page')});
+{res.send('Hello home page, <img src="/Certification.jpg">')});
+app.use(express.static('public'));
 app.listen(3000, () => {
   console.log('Connected 3000 port!')
 });
